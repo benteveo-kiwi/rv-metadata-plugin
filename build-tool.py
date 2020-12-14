6646cc30b7432a27f4b7db9154a59df680c6eec4
Gabriela Roque Lopez <gabrielamelian@gmail.com>
2020-12-14 13:42:25 +1300

    Adds the names and docstring for the main functions to be implemented

--- build-tool.py
+++ build-tool.py
@@ -10,6 +10,46 @@ __license__ = "GPL-3.0"
 import argparse
 
 
+def get_package_info():
+    """
+    Looks up the Package name, Author and Version from the plugin PACKAGE file
+
+    Returns:
+        tuple (str, str, float): The name, author and latest version of this plugin
+
+    """
+
+
+def build():
+    """
+    Creates a 'build' folder and packages the script files into a zip .rvpkg file
+
+    Returns:
+        string: The packaged file location on disk as a string
+    """
+    return
+
+
+def install(rvpkg_file_location):
+    """
+    Runs the 'rvpkg' program to add and install the current rvpkg file that it's inside the build folder
+    It takes into account the current OS to find the correct folders
+
+    Args:
+        rvpkg_file_location (str): The location on disk of the rvpkg file to install
+
+    Returns:
+
+    """
+    return
+
+
+def clean_existing_installation():
+    """
+    Removes the contents of the 'build' folder
+    """
+
+
 def main(args):
     """ Main entry point of the app """
     print("hello world")
