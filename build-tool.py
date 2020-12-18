4492e00b5bca1c8f62a149065018044efd83c57b
Roque Lopez <gabrielamelian@gmail.com>
2020-12-18 14:12:00 +1300

    Implements build function to create zip file. Adds some constants with locations on disk

--- build-tool.py
+++ build-tool.py
@@ -8,16 +8,47 @@ __version__ = "0.1.0"
 __license__ = "GPL-3.0"
 
 import argparse
+import platform
+import os
+import zipfile
+
+
+# Location of the rvpkg app on different platforms
+MACOS_RVPKG = "/Applications/RV.app/Contents/MacOS/rvpkg"
+WINDOWS_RVPKG = "C:\Program Files\Shotgun\RV-2021.0.0\bin\rvpkg.exe"
+LINUX_RVPKG = "rvpkg"
+
+# Location of the installation directory on different platforms
+MACOS_PATH_TO_AREA = "~/Library/Application\ Support/RV"
+# TODO: get the home directory with python?
+WINDOWS_PATH_TO_AREA = "C:\\Users\\Gabita\\AppData\\Roaming\\RV\\Packages"
+LINUX_PATH_TO_AREA = "~/.rv/Packages"
+
+PLATFORM_NAME = platform.system()
 
 
 def get_package_info():
     """
-    Looks up the Package name, Author and Version from the plugin PACKAGE file
+    Looks up the Package name and Version from the plugin PACKAGE file
 
     Returns:
-        tuple (str, str, float): The name, author and latest version of this plugin
+        tuple (str, float): The name and latest version of this plugin
 
     """
+
+
+def zipdir(path, rvpkg_file):
+    """
+    It grabs all the files inside the path folder and adds them to the zip rvpkg file
+
+    Args:
+        path: Folder where the plugin files are stored
+        rvpkg_file: ZipFile instance of the rvpkg file
+    """
+
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            rvpkg_file.write(filename=os.path.join(root, file), arcname=file)
 
 
 def build():
@@ -27,6 +58,14 @@ def build():
     Returns:
         string: The packaged file location on disk as a string
     """
+    try:
+        os.mkdir("build")
+    except FileExistsError:
+        print("Build folder already exists")
+
+    with zipfile.ZipFile("build\\metadata_finder-0.1.0.rvpkg", mode='w') as rvpkg_file:
+        zipdir("plugin", rvpkg_file)
+
     return
 
 
@@ -46,14 +85,15 @@ def install(rvpkg_file_location):
 
 def clean_existing_installation():
     """
-    Removes the contents of the 'build' folder
+    Removes the contents of the 'build' folder and
     """
 
 
 def main(args):
     """ Main entry point of the app """
-    print("hello world")
-    print(args)
+
+    if args.build:
+        build()
 
 
 if __name__ == "__main__":
