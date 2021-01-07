ad3855f6f4861b1c1d63b383df578084af020444
Roque Lopez <gabrielamelian@gmail.com>
2021-01-07 14:07:26 +1300

    Adds better documentation to argparser and removes un-used flag

--- build-tool.py
+++ build-tool.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """
-This is a script to assist with RV's package development in order to iterate and deploy easily
+This is a script to assist with RV's package development in order to iterate and deploy easily.
+It searches inside the 'plugin' folder and adds the contents to a zip ".rvpkg" file.
+It can then install and add the rvpkg to the RV installation.
+It can start/restart RV to pick up the latest changes.
 """
 
 __author__ = "Gabriela Roque Lopez"
@@ -130,8 +133,8 @@ def main(args):
 
 
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    parser = argparse.ArgumentParser()
+
+    parser = argparse.ArgumentParser(description=__doc__)
 
     parser.add_argument("-b", "--build",
                         action="store_true",
@@ -153,12 +156,7 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--test",
                         action="store_true",
                         default=False,
-                        help="Run unit tests for plugin, defaults to False")
-
-    parser.add_argument("-v", "--verbose",
-                        action="count",
-                        default=0,
-                        help="Verbosity (-v, -vv, etc)")
+                        help="Run unit tests for plugin, defaults to False. NOT IMPLEMENTED")
 
     args = parser.parse_args()
     main(args)
